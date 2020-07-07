7487c44b01d36c0b6ead9d52fd2698371e0498eb
i323846 <I323846@INLN56605656A.amer.global.corp.sap>
2020-07-07 09:33:39 +0530

    update.

--- src/main/java/com/example/demo/retry/configuration/Config.java
+++ src/main/java/com/example/demo/retry/configuration/Config.java
@@ -1,9 +1,13 @@
 package com.example.demo.retry.configuration;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.retry.annotation.EnableRetry;
 
 @Configuration
+@Profile({ "cloud", "local", "default" })
+@PropertySource("classpath:default.properties")
 @EnableRetry(proxyTargetClass = true)
 public class Config {
 
--- src/main/java/com/example/demo/retry/repository/TestRepository1.java
+++ src/main/java/com/example/demo/retry/repository/TestRepository1.java
@@ -9,10 +9,14 @@ import com.example.demo.retry.exceptions.ValueException;
 @Repository
 public class TestRepository1 {
 
-	/* This will retry 3 times */
+	/*- This will retry 3 times 
 	@Retryable(value = { NumberFormatException.class, ValueException.class }, //
 			maxAttempts = 3, //
 			backoff = @Backoff(delay = 1000))
+	*/
+	@Retryable(value = { NumberFormatException.class, ValueException.class }, //
+			maxAttemptsExpression = "#{${retry.max.attempts}}", //
+			backoff = @Backoff(delayExpression = "#{${retry.backoff.delay}}"))
 	public String getResult(String id) throws ValueException {
 		System.out.println("test service is called with id " + id);
 
