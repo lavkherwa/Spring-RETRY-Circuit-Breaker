93e41272d70f1ef8b674a1410439ee596eaebeb7
i323846 <I323846@INLN56605656A.amer.global.corp.sap>
2020-07-07 10:04:03 +0530

    update

--- src/main/java/com/example/demo/retry/configuration/Config.java
+++ src/main/java/com/example/demo/retry/configuration/Config.java
@@ -1,13 +1,11 @@
 package com.example.demo.retry.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.retry.annotation.EnableRetry;
 
+//@Profile({ "cloud", "local", "default" })
+//@PropertySource("classpath:default.properties")
 @Configuration
-@Profile({ "cloud", "local", "default" })
-@PropertySource("classpath:default.properties")
 @EnableRetry(proxyTargetClass = true)
 public class Config {
 
